e66d52440df19ebdaf062a9a06fc3007ca960db7
ftedhuji <gs1syntaxia@hotmail.com>
2024-05-14 15:15:57 +0700

    start

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-const titleCase = (str) =>
-  str.toLowerCase().replace(/\b\w/g, (char) => char.toUpperCase());
+function isValidBST(root) {
+  return isValid(root, null, null);
+}
+function isValid(node, min, max) {
+  if (!node) return true;
+  if ((min !== null && node.val <= min) || (max !== null && node.val >= max))
+    return false;
+  return (
+    isValid(node.left, min, node.val) && isValid(node.right, node.val, max)
+  );
+}
